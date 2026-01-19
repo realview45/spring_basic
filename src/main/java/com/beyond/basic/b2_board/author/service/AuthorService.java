e4baf36ed015cb62a9cd2d0a5db5a74ab4f85518
realview45 <kjink0125@gmail.com>
2026-01-19 16:51:16 +0900

    jdbc를 이용한 insert, select (회원가입, 조회)

--- src/main/java/com/beyond/basic/b2_board/author/service/AuthorService.java
+++ src/main/java/com/beyond/basic/b2_board/author/service/AuthorService.java
@@ -1,83 +1,38 @@
 package com.beyond.basic.b2_board.author.service;
-
 import com.beyond.basic.b2_board.author.domain.Author;
 import com.beyond.basic.b2_board.author.dtos.AuthorCreateDto;
 import com.beyond.basic.b2_board.author.dtos.AuthorDetailDto;
 import com.beyond.basic.b2_board.author.dtos.AuthorListDto;
+import com.beyond.basic.b2_board.author.repository.AuthorJdbcRepository;
 import com.beyond.basic.b2_board.author.repository.AuthorMemoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 //Component어노테이션을 통해 싱글통(단하나의)객체가 생성되고, 스프링에 의해 스프링컨텍스트에서 관리
 @Service
-//3반드시 초기화되어야하는필드(final변수 등)를 대상으로 생성자를 자동생성
-//@RequiredArgsConstructor
 public class AuthorService {
-//    의존성주입(DI)방법1. 필드주입 : Autowired어노테이션사용 (간편방식)
-//    @Autowired
-//    의존성주입(DI)방법2. 생성자주입방식(가장많이 사용되는 방식)
-//    장점1)final을 통해 상수로 사용가능(안정성향상) new가 안됨 1은 초기화못함
-//    장점2)다형성 구현가능(interface사용가능) 1은 자식중에 누구넣을지 모르기때문
-//    장점3)순환참조방지(컴파일타임에 에러check)이건 123다된다
-    private final AuthorMemoryRepository authorMemoryRepository;
+    private final AuthorJdbcRepository authorRepository;
 //    생성자가 하나밖에 없을때에는 Autowired생략가능 내가 짤때는 붙여주는게 성능이 좋다라고 알려짐
     @Autowired
-    public AuthorService(AuthorMemoryRepository authorMemoryRepository){
-        this.authorMemoryRepository = authorMemoryRepository;
+    public AuthorService(AuthorJdbcRepository authorRepository){
+        this.authorRepository = authorRepository;
     }
-//    public AuthorService(){
-//        this.authorRepository = new AuthorRepository();
-//    }
-
-//    의존성주입방법3. RequiredArgsConstructor어노테이션 사용방법 장점1가능
-//반드시 초기화되어야 하는 필드를 선언하고, 위 어노테이션 선언시 생성자주입방식으로 의존성이 주입됨
-//        단점 : 다형성 설계는 불가
-//    private final AuthorRepository authorRepository;
 
     public void save(AuthorCreateDto dto){
-//        방법1. 객체 직접 조립
-//        1-1)생성자만을 활용한 객체 조립
-//        Author author = new Author(null, dto.getName(), dto.getEmail(), dto.getPassword());
-//        1-2)Builder패턴을 활용한 객체 조립(표준)
-//        장점 : 1)매개변수 개수의 유연성 2)매개변수 순서의 유연성
-//        Author author = Author.builder()
-//                .email(dto.getEmail())
-//                .name(dto.getName())
-//                .password(dto.getPassword())
-//                .build();
-//        방법2. toEntity, fromEntity 패턴을 통한 객체 조립
-//        객체조립이라는 반복적인 작업을 별도의 코드로 떼어내 공통화
-        authorMemoryRepository.save(dto.toEntity());
+        authorRepository.save(dto.toEntity());
     }
     public List<AuthorListDto> findAll(){
-        return authorMemoryRepository.findAll().stream()
+        return authorRepository.findAll().stream()
                 .map(a->AuthorListDto.fromEntity(a))
                 .collect(Collectors.toList());//리스트로 바꿔준다.
-//        List<Author> authorList = authorRepository.findAll();
-//        List<AuthorListDto> dtoList = new ArrayList<>();
-//        for(Author a : authorList){
-//            dtoList.add(new AuthorListDto(a.getId(),a.getName(),a.getEmail()));
-//        }
-//        return dtoList;
     }
     public AuthorDetailDto findById(Long id){
-        Optional<Author> optAuthor = authorMemoryRepository.findById(id);
+        Optional<Author> optAuthor = authorRepository.findById(id);
                                                     //entitynotfound jpa구리
         Author author = optAuthor.orElseThrow(()->new NoSuchElementException("entity is not found"));
-        //dto조립
-//        fromEntity는 아직 dto객체가 만들어지지 않은 상태이므로 static메서드로 설계
-//        Author객체에 만들어버리면 확장성이 떨어진다 기반이 Author뿐만이아닐수있기때문구리!
-//        AuthorDetailDto dto = AuthorDetailDto.builder()
-//                .id(author.getId())
-//                .name(author.getName())
-//                .email(author.getEmail())
-//                .password(author.getPassword())
-//                .build();
         return AuthorDetailDto.fromEntity(author);
     }
 }
