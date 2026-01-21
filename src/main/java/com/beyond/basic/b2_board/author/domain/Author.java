6c978c4c9835f23963dc10c4842e42e690347be8
realview45 <kjink0125@gmail.com>
2026-01-21 10:39:24 +0900

    순수jpa (spring data jpa읭 근간)를 통한 db접근

--- src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
+++ src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
@@ -1,5 +1,7 @@
 package com.beyond.basic.b2_board.author.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 //저자(Author)엔티티 도메인 -name, email, password
@@ -8,7 +10,10 @@ import lombok.*;
 @Getter @ToString
 //빌더패턴은 AllArgs생성자 기반으로 동작한다.
 @Builder
+//JPA에게 Entity관리를 위임하기 위한 어노테이션
+@Entity
 public class Author {
+    @Id //pk설정
     private Long id;
     private String name;
     private String email;
---
+++ src/main/java/com/beyond/basic/b2_board/author/repository/AuthorJpaRepository.java
@@ -0,0 +1,26 @@
+package com.beyond.basic.b2_board.author.repository;
+
+import com.beyond.basic.b2_board.author.domain.Author;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AuthorJpaRepository {
+    void save(Author author){
+
+    }
+    Optional<Author> findById(Long id){
+        return null;
+    }
+    Optional<Author> findByEmail(String email){
+        return null;
+    }
+    List<Author> findAll(){
+        return null;
+    }
+    void delete(Long id){
+
+    }
+}
