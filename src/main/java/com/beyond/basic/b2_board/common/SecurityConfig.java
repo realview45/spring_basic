b08b1437038b6436007c4d5bc1a33eb428bfacae
realview45 <kjink0125@gmail.com>
2026-01-23 14:43:12 +0900

    Bean설정 create가능

---
+++ src/main/java/com/beyond/basic/b2_board/common/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.beyond.basic.b2_board.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+//    내가 만든 클래스와 객체는 @Component, 외부 클래스(라이브러리)를 활용한 객체는 @Component
+//    @Component는 클래스 위에 붙여 클래스기반에 객체를 싱글톤객체로 생성.
+//    @Bean은 메서드위에 붙여 Return되는 객체를 싱글톤객체로 생성.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+//                a->AbstractHttpConfigurer(a)구리
+//                csrf공격(일반적으로 쿠키를 활용한 (세션방식에서 활용파일구리) 공격)에 대한 방어 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+//                http basic은 email/pw를 인코딩하여 인증(전송)하는 간단한 인증방식. 비활성화.
+                .httpBasic(AbstractHttpConfigurer::disable)
+//                세션로그인방식 비활성화
+                .sessionManagement(a->a.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                지정한 특정url을 제외한 모든 요청에 대해서 authenticated(인증처리)하겠다라는 의미
+                .authorizeHttpRequests(a->a.requestMatchers(
+                        "/author/create", "/author/login").permitAll().anyRequest().authenticated())
+                .build();
+    }
+}
