e993cd0ae5f970903bebf5d02cf36f2a689ae2ba
realview45 <kjink0125@gmail.com>
2026-01-23 17:35:54 +0900

    필터링전

--- src/main/java/com/beyond/basic/b2_board/common/auth/SecurityConfig.java
+++ src/main/java/com/beyond/basic/b2_board/common/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.beyond.basic.b2_board.common.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 public class SecurityConfig {
-//    내가 만든 클래스와 객체는 @Component, 외부 클래스(라이브러리)를 활용한 객체는 @Component
+//    private final JwtTokenFilter jwtTokenFilter;
+//    @Autowired
+//    public SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+//        this.jwtTokenFilter = jwtTokenFilter;
+//    }
+
+    //    내가 만든 클래스와 객체는 @Component, 외부 클래스(라이브러리)를 활용한 객체는 @Component
 //    @Component는 클래스 위에 붙여 클래스기반에 객체를 싱글톤객체로 생성.
 //    @Bean은 메서드위에 붙여 Return되는 객체를 싱글톤객체로 생성.
     @Bean
@@ -23,6 +32,8 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
 //                세션로그인방식 비활성화
                 .sessionManagement(a->a.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                token을 검증하고, Authentication객체 생성
+//                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
 //                지정한 특정url을 제외한 모든 요청에 대해서 authenticated(인증처리)하겠다라는 의미
                 .authorizeHttpRequests(a->a.requestMatchers(
                         "/author/create", "/author/login").permitAll().anyRequest().authenticated())
