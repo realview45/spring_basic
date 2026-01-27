923a2ddf7157c9a340667ecc341f567e9a4a0c62
realview45 <kjink0125@gmail.com>
2026-01-27 14:35:27 +0900

    로그레벨 info: info미만은 안보겠다. debug: trace는 안보임 trace > debug > info > error

--- src/main/java/com/beyond/basic/b2_board/common/controller/LogController.java
+++ src/main/java/com/beyond/basic/b2_board/common/controller/LogController.java
@@ -1,15 +1,28 @@
 package com.beyond.basic.b2_board.common.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogController {
+//    logback 객체를 만드는 방법1.
+    private static Logger logger = LoggerFactory.getLogger(LogController.class);
+
+
     @GetMapping("/log/test")
     public String logTest(){
 //        system println의 문제점
 //        1)출력의 성능이 떨어짐 2)로그분류작업 불가
         System.out.println("hello world");
+//        가장 많이 사용되는 로그라이브러리 : logback
+        logger.trace("trace로그입니다.");
+        logger.debug("debug로그입니다.");
+        logger.info("info로그입니다.");
+        logger.error("error로그입니다.");
+        logger.info("hello world");
+
         return "OK";
     }
 }
