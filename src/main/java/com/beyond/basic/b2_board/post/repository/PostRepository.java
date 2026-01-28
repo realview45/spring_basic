ef0cd741e6bc44f6943396ff4a4df9dce772fb00
realview45 <kjink0125@gmail.com>
2026-01-28 11:16:11 +0900

    Pageable에서의 N+1문제 fetch join이 자동으로 된다.

--- src/main/java/com/beyond/basic/b2_board/post/repository/PostRepository.java
+++ src/main/java/com/beyond/basic/b2_board/post/repository/PostRepository.java
@@ -5,6 +5,7 @@ import com.beyond.basic.b2_board.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -30,6 +31,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 //    Page객체 안에는 content(List<Post>), totalPages, totalElement등의 정보 포함
 //    Page<Post> findAllByDelYn(Pageable pageable, String delYn);
 //    검색 + 페이징처리까지 할 경우, 아래와 같이 매개변수 선언.(Specification, Pageable 순서 - SimpleJpaRepository에서 정의)
+    @EntityGraph(attributePaths = "author")//->fetch join 어노테이션
     Page<Post> findAll(Specification<Post> specification, Pageable pageable);
 
 }
