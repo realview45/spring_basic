ac102446b5d4b70fc3d7d3b8cbbfa55383545fc1
realview45 <kjink0125@gmail.com>
2026-01-28 14:50:57 +0900

    배치사용시 대용량데이터 순차실행 정합성 유지(스케줄러를 통해 정의한 BatchJob을 실행)schema-mariadb.sql 복사 db에서 실행(ctrl+shift+Enter)(workbench) ex)복잡한 긴흐름의 작업 점검, 스케줄러를 통해 트리거 설정

---
+++ src/main/java/com/beyond/basic/b2_board/post/service/PostBatchScheduler.java
@@ -0,0 +1,32 @@
+package com.beyond.basic.b2_board.post.service;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostBatchScheduler {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+    @Autowired
+    public PostBatchScheduler(JobLauncher jobLauncher, Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void batchScheduler(){
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())//각 실행 job들에 id값(시간)
+                    .toJobParameters();
+            jobLauncher.run(job, jobParameters);
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+    }
+}
